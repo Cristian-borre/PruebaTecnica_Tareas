a01c4ea945ea525eed2ef433d4986644cf92bd60
Cristian Borré <129191791+Cristian-borre@users.noreply.github.com>
2023-09-02 01:51:27 +0000

    commit

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './inicio/inicio.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Front';
